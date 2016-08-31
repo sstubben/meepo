7e80386fdde553305f7028c391b99757d39e6fba
Simon Stubben <simonstubben@gmail.com>
2016-08-31 10:56:16 +0200

    Added feature tests for authorization. Added devise - authenticate_user! to team and user controllers. Need to check User controller for unnecessary/duplicate actions

--- spec/features/authentications_spec.rb
+++ spec/features/authentications_spec.rb
@@ -22,26 +22,65 @@ RSpec.feature "Authentications:", type: :feature do
         }.by(1)
       end
 
+      it 'authorizes the user and redirects to root' do
+        visit "/"
+        click_link "Sign up"
+        fill_in "user_email", with: "test@user.com"
+        fill_in "user_password", with: "test1234"
+        fill_in "user_password_confirmation", with: "test1234"
+        option = page.find(:xpath, "//*[@id='user_team_id']/option[2]").text
+        page.select(option,from:'user_team_id')
+        click_button 'Sign up'
+        expect(page).to have_current_path(root_path)
+      end
 
-      it 'authorizes the user and redirects to root'
-
-      it 'shows a "Register succesful" flash message to the user'
+      it 'shows a "Register succesful" flash message to the user' do
+        visit "/"
+        click_link "Sign up"
+        fill_in "user_email", with: "test@user.com"
+        fill_in "user_password", with: "test1234"
+        fill_in "user_password_confirmation", with: "test1234"
+        option = page.find(:xpath, "//*[@id='user_team_id']/option[2]").text
+        page.select(option,from:'user_team_id')
+        click_button 'Sign up'
+        expect(page).to have_content('You have signed up successfully.')
+      end
 
     end
 
   end
 
   describe "user login" do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+    end
+
     context "when User inputs correct email and password" do
-      it 'authorizes the user and redirects to root'
+      it 'authorizes the user and redirects to root' do
+        visit "/"
+        fill_in "user_email", with: @user.email
+        fill_in "user_password", with: '12345678'
+        click_button 'Log in'
+        expect(page).to have_current_path(root_path)
+      end
 
 
-      it 'shows a "Login succesful" flash message to the user'
+      it 'shows a "Login succesful" flash message to the user' do
+        visit "/"
+        fill_in "user_email", with: @user.email
+        fill_in "user_password", with: '12345678'
+        click_button 'Log in'
+        expect(page).to have_content('Signed in successfully.')
+      end
 
     end
     context "when User inputs incorrect email or password" do
-      it 'shows a "Wrong input message"'
-
+      it 'shows a "Wrong input message"' do
+        visit "/"
+        fill_in "user_email", with: @user.email
+        click_button 'Log in'
+        expect(page).to have_content('Invalid Email or password.')
+      end
     end
 
   end
